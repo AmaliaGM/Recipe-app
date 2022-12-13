b56aa39f5caacca2810291238d70fe724d0a92b3
Omachi John <omachi.john@yahoo.com>
2022-12-13 09:08:51 -0800

    Add after signin path to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,18 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
-        before_action :update_allowed_parameters, if: :devise_controller?
+    before_action :update_allowed_parameters, if: :devise_controller?
 
-        protected
-
-        def update_allowed_parameters
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
-        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
-        end
+    def after_sign_in_path_for(_resource)
+      users_url
+    end
+  
+    protected
+  
+    def update_allowed_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :bio, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:name, :bio, :email, :password, :password_confirmation, :current_password)
+      end
+    end
 end
