2dca87431c0cc26447e926f2a7113926c82495db
Omachi John <omachi.john@yahoo.com>
2022-12-14 16:53:45 -0800

    Display all public recipe lis

--- db/migrate/20221213180858_create_foods.rb
+++ db/migrate/20221213180858_create_foods.rb
@@ -3,7 +3,7 @@ class CreateFoods < ActiveRecord::Migration[7.0]
     create_table :foods do |t|
       t.string :name
       t.string :measurement_unit
-      t.decimal :price
+      t.integer :price
       t.integer :quantity
       t.references :user, null: false, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_13_194313) do
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.string "measurement_unit"
-    t.decimal "price"
+    t.integer "price"
     t.integer "quantity"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
