124a05cf3d6c96392b9025c0e46605be49d8ad4a
Omachi John <omachi.john@yahoo.com>
2022-12-15 12:33:42 -0800

    write test cases for all routes

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -15,5 +15,27 @@ RSpec.describe 'Foods', type: :request do
      it 'should return response status correct (ok)' do
           expect(response).to have_http_status('200')
         end
+
+        it 'respons to html' do
+          expect(response.content_type).to include 'text/html'
+        end
 end
+          describe 'GET /new' do
+          before do
+          sign_in user
+          get new_food_path
+          end
+
+     it 'should return response status correct (ok)' do
+               expect(response).to have_http_status('200')
+             end
+
+      it 'respons to html' do
+               expect(response.content_type).to include 'text/html'
+             end
+
+     it 'should include correct placeholder' do
+               expect(response.body).to include('application')
+             end
+          end
 end
