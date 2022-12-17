4c443ae49f100ce75588322ceb51db49ca67b797
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-12-17 14:20:01 -0300

    render-build update

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
-bundle exec rake db:seed
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
+bundle exec rails db:seed
