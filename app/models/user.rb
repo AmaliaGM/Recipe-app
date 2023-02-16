20c4f25122ce1f45800d8d05087b943667cca120
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2023-02-16 12:32:21 -0300

    devise update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+   devise :database_authenticatable, :registerable,
+      :recoverable, :rememberable, :validatable
 
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
